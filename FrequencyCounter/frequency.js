9f4c38735b833041d9082834e4bacfb5cbd8cd01
mahmud <mahmudarif1175@gmail.com>
2019-02-17 20:01:40 +0600

    another solution added

--- FrequencyCounter/frequency.js
+++ FrequencyCounter/frequency.js
@@ -1,3 +1,5 @@
+// naiv solution
+
 function same(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
@@ -11,5 +13,34 @@ function same(arr1, arr2) {
     arr2.splice(correctIndex, 1)
   }
 
+  return true; 
+}
+
+
+// refactor 
+
+function same(arr1, arr2) {
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
+
+  const frequncyCounter1 = {}; 
+  const frequncyCounter2 = {}; 
+  for (val of arr1) {
+    frequncyCounter1[val] = (frequncyCounter1[val] || 0) + 1; 
+  }
+  for (val of arr2) {
+    frequncyCounter2[val] = (frequncyCounter2[val] || 0) + 1;
+  }
+
+  for (key in frequncyCounter1) {
+    if (!(key ** 2 in frequncyCounter2)) {
+      return false; 
+    }
+    if (frequncyCounter1[key] !== frequncyCounter2[key ** 2]) {
+      return false; 
+    }
+
+  }
   return true; 
 }
