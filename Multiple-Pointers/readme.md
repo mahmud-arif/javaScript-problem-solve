8d2a0c10ada86da076e3c719919294a9f0307235
mahmud <mahmudarif1175@gmail.com>
2019-02-17 22:56:33 +0600

    added mulple pointer problem and solution

---
+++ Multiple-Pointers/readme.md
@@ -0,0 +1,10 @@
+# MULTIPLE PINTERS
+  > Creating pointers or valuse that correspond to an index or position and move towards the beginning, end or middle based on a certain condition
+
+## AN EXAMPLE 
+  Write a function called sumZero which accepts a sorted array of integers. the function should find the first pair where the sum is 0; return an array that includes both values that sum to zero or undefined if a pair does not exist
+
+```javascript 
+ sumZero([-3,-2,-1,0,1,2,3]) // [-3, 3]
+ sumZero([-2,0,1,3]) //undefined
+ sumZero([1,2,3]) // undefined
---
+++ Multiple-Pointers/sumZero.js
@@ -0,0 +1,31 @@
+// naiv solution
+
+function sumZero(arr) {
+  for (let i = 0; i < arr.length; i++){
+    for (let j = i+1; j < arr.length; j++){
+      if (arr[i] + arr[j] === 0) {
+        return [arr[i], arr[j]]; 
+      }
+    }
+  } 
+}
+
+// refector solution
+
+function sumZero(arr) {
+  let left = 0; 
+  let right = arr.length - 1; 
+  while (left < right) {
+    let sum = arr[left] + arr[right]; 
+    if (sum === 0) {
+      return [arr[left], arr[right]]; 
+    } else if (sum > 0) {
+      right--;
+    } else {
+      left++; 
+    }
+  }
+}
+
+console.log(sumZero([-3, -2, -1, 0, 1, 2, 3])); 
+console.log(sumZero([-2, 1, 0, 3])); 
