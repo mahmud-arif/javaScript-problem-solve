bf6826ca124c48dcb2a75719ba230428971161b7
mahmud <mahmudarif1175@gmail.com>
2019-03-31 11:55:40 +0600

    bubble sort solution

---
+++ Sorting-Algo/BubbleSort/bubbleSort.js
@@ -0,0 +1,27 @@
+function bubbleSort(arr) {
+  for (let i = arr.length; i > 0; i--){
+    for (let j = 0; j < i - 1; j++){
+      if (arr[j] > arr[j + 1]) {
+        let temp = arr[j]; 
+        arr[j] = arr[j + 1]; 
+        arr[j + 1] = temp; 
+      }
+    }
+  }
+  return arr; 
+}
+
+//es6 version swap
+function bubbleSort(arr) {
+  const swap = (arr, idx1, idx2) => {
+    [arr[idx1], arr[idx2]] = [arr[idx2], arr[idx1]]; 
+  }
+  for (let i = arr.length; i > 0; i--) {
+    for (let j = 0; j < i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        swap(arr, j, j + 1); 
+      }
+    }
+  }
+  return arr;
+}
--- Sorting-Algo/BubbleSort/readme.md
+++ Sorting-Algo/BubbleSort/readme.md
@@ -3,3 +3,12 @@
 > - A sorting algorithm where the largest values bubble up to the top 
 
 ![bubble sort!](../../assets/bubble.png "buble sort")
+
+
+### Pesudo code 
+
+>
+> - start looping from with a variable called i the end of the array towards the beginning
+> - start an inner loop with a variable called j from the beginning until i-1
+> - if array arr[j] grater than arr[j+1], swap those two values
+> - return the stored array 
