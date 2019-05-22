5b519bfbbc01c413120b416cd691eb2ff0b1166e
mahmud <mahmudarif1175@gmail.com>
2019-05-23 04:09:46 +0600

    added DFSr

--- DataStructure/Tree_Traversal/TreeTraversal.js
+++ DataStructure/Tree_Traversal/TreeTraversal.js
@@ -72,9 +72,32 @@ class BinarySearchTree {
     function traverse(node) {
       data.push(node.value); 
       if (node.left) traverse(node.left); 
-      if(node.right) traverse(node.left)
+      if(node.right) traverse(node.right)
     }
     traverse(this.root); 
     return data;
   }
+  DFSPostOrder() {
+    let data = [];
+
+    function traverse(node) {
+      if (node.left) traverse(node.left);
+      if (node.right) traverse(node.right)
+      data.push(node.value);
+    }
+    traverse(this.root);
+    return data;
+  }
+   DFSIntOrder() {
+     let data = [];
+
+     function traverse(node) {
+       if (node.left) traverse(node.left);
+       data.push(node.value);
+       if (node.right) traverse(node.right); 
+       
+     }
+     traverse(this.root);
+     return data;
+   }
 }
--- DataStructure/Tree_Traversal/readme.md
+++ DataStructure/Tree_Traversal/readme.md
@@ -31,3 +31,15 @@
    * If the node has a right property, call the helper function with the right property on the node
 * Invoke the helper function with the current variable
 * Return the array of values
+
+# DFS PostOrder (left>right>root)
+
+  **Speps**- Recursively
+* Create a variable to store the values of nodes visited
+* Store the root of the BST in a variabel called cureent
+* Write a helper function which accepts a node
+   * If the node has a left property, call the helper function with the left property on the node
+   * If the node has a right property, call the helper function with the right property on the node
+   * Push the value of the node to the variable that store the values
+* Invoke the helper function with the current variable
+* Return the array of values
