7a80864605c603a2583fc5e9cb73917dc8130cca
mahmud <mahmudarif1175@gmail.com>
2019-05-23 03:04:37 +0600

    added BFS & DFS PreOrder

---
+++ DataStructure/Tree_Traversal/TreeTraversal.js
@@ -0,0 +1,80 @@
+class Node {
+  constructor(value) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
+
+class BinarySearchTree {
+  constructor() {
+    this.root = null;
+  }
+  insert(value) {
+    let newNode = new Node(value);
+    if (this.root === null) {
+      this.root = newNode;
+      return this;
+    } else {
+      let current = this.root;
+      while (true) {
+        if (value <= current.value) {
+          if (this.current.left === null) {
+            this.current.left = newNode;
+            return this;
+          } else {
+            current = this.current.left;
+          }
+        } else {
+          if (value > current.value) {
+            if (this.current.right === null) {
+              this.current.right = newNode;
+              return this;
+            } else {
+              current = this.current.right;
+            }
+          }
+        }
+      }
+    }
+  }
+   find(value) {
+     if (this.root === null) return false;
+     let current = this.root,
+       found = false;
+     while (current && !found) {
+       if (val < current.value) {
+         current = current.left;
+       } else if (value > current.value) {
+         current = current.right;
+       } else {
+         return true;
+       }
+     }
+     return false;
+   }
+  
+  BFS() {
+    let node = this.root,
+      queue = [],
+      data = [];
+    queue.push(node); 
+    while (queue.length) {
+      node = queue.shift();
+      data.push(node.val); 
+      if (node.left) queue.push(node.left); 
+      if (node.right) queue.push(node.right); 
+    }
+    return data; 
+  }
+  DFSPreOrder() {
+    let data = []; 
+    function traverse(node) {
+      data.push(node.value); 
+      if (node.left) traverse(node.left); 
+      if(node.right) traverse(node.left)
+    }
+    traverse(this.root); 
+    return data;
+  }
+}
---
+++ DataStructure/Tree_Traversal/readme.md
@@ -0,0 +1,33 @@
+# TRAVERSING A TREE
+
+
+
+  **Two** **ways**
+* Breath First Search
+* Depth First Search
+   * In Order
+   * Pre Order
+   * Post Order
+
+
+### BFS
+
+* Create a queue (this can be an array) and variable to store the values of nodes visited
+* Place the root node in the queue
+* Loop as long there is anything in the queue
+    * Dequeue a node from the queue and push the value of the node into the variable that stores the nodes
+    * If there is a left property on the node dequeued - add it to the queue
+    * If there is a rigth property on the node dequeued - add it to the queue
+* Return the variable that stores the values
+
+# DFS PreOrder(root>left>right)
+
+  **Speps**- Recursively
+* Create a variable to store the values of nodes visited
+* Store the root of the BST in a variabel called cureent
+* Write a helper function which accepts a node
+   * Push the value of the node to the variable that store the values
+   * If the node has a left property, call the helper function with the left property on the node
+   * If the node has a right property, call the helper function with the right property on the node
+* Invoke the helper function with the current variable
+* Return the array of values
